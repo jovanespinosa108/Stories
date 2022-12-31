572cc05bde2ff2ed756dd1979a8c3bb0e13ded5a
jovanespinosa108@gmail.com <jovanespinosa108@gmail.com>
2022-12-31 17:37:51 -0600

    generated stories project

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <div>
+          Stories For Kids
+        </div>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
